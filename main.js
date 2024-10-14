88b56d3230eff9ec96e73b8dca79e53541341f6f
Navitha Venuraka <navitharathnayake08@gmail.com>
2024-10-14 20:34:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+ <script>
+    const menuBtn = document.getElementById('menu-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuBtn.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
+
+
+
+
+
+
+
+
+
+
+
+  </script>
